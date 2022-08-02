ea062d46298f560de533d73c9abbb5b58154c1de
7r4d3r <7r4d3r>
2022-08-02 00:53:44 -0700

    commit

---
+++ precache-manifest.b5cf63ff6e08fbfcd5441d57cc090203.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb277c84ae4853256f9853168e17698b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4069c1b41d0e38b90e83",
+    "url": "/static/css/2.8389b08b.chunk.css"
+  },
+  {
+    "revision": "e0d74ca87526fb630cd4",
+    "url": "/static/css/main.2187f7ce.chunk.css"
+  },
+  {
+    "revision": "4069c1b41d0e38b90e83",
+    "url": "/static/js/2.a63b4f6a.chunk.js"
+  },
+  {
+    "revision": "4a4bcbcfcc085c06cd762672e644f1f2",
+    "url": "/static/js/2.a63b4f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d74ca87526fb630cd4",
+    "url": "/static/js/main.50c9710d.chunk.js"
+  },
+  {
+    "revision": "ff8347c2a84f5138b487",
+    "url": "/static/js/runtime-main.766003da.js"
+  },
+  {
+    "revision": "e422bd20ceb2e3a763ec40dcf4f8ae57",
+    "url": "/static/media/logo.e422bd20.svg"
+  }
+]);
